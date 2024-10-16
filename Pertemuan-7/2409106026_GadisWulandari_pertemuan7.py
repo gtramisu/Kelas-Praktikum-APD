dbba5c593ed7a7f36856620f4c9c975eab7ff985
adiss <naagadis@gmail.com>
2024-10-16 08:45:17 +0800

    Add files via upload

---
+++ Pertemuan-7/2409106026_GadisWulandari_pertemuan7.py
@@ -0,0 +1,118 @@
+# fungsi = mengembalikan nilai
+# prosedur = tidak mengembalikan nilai
+
+# def menu():
+#     print("Menu pilihan")
+#     print("1. Tambah")
+#     print("2. Kurang")
+#     print("3. Kali")
+#     print("4. Bagi")
+
+# menu()
+
+# def salam():
+# nama = "ridho"
+#     print ("Selamat Pagi", nama)
+#     print ("Selamat Pagi, FT Muda")
+# def kali():
+#     x = 6*4
+#     print(x)
+
+# salam()
+# kali()
+
+# def salam(nama):
+#     print("selamat pagi", nama)
+
+# # print(salam("ridho"))
+# salam("ridho")
+
+# a = 6
+# b = 4
+# a = input("Masukkan angka pertama: ")
+# b = input("Masukkan angka kedua: ")
+# def tambah(a,b):
+#     hasil = a + b
+#     print(hasil)
+
+# tambah(a,b)
+# tambah(6,4)
+
+# def penjumlahan():
+# a = int(input("Masukkan angka pertama: "))
+# b = int(input("Masukkan angka kedua: "))
+#     a = 6
+#     b = 4
+#     hasil = a + b
+#     print(hasil)
+
+# penjumlahan()
+
+# fungsi
+# a = int (input("Masukkan angka pertama: "))
+# b = int (input("Masukkan angka kedua: "))
+# print(a)
+# print(b)
+
+# def tambah(a,b):
+#     hasil = a + b 
+#     return hasil
+
+# print(tambah(a,b))
+# print(tambah(5,9))
+# print(hasil) = gabisaaa!!
+
+# nama = "shandy" global
+
+# def salam():
+#     nama = "ibnu" lokal
+#     print(nama)
+
+# salam() prosedur
+# print(nama) global
+
+# rumus sisi x sisi
+# def luas_persegi (sisi):
+#     luas = sisi * sisi
+#     return luas
+
+# rumus sisi x sisi x sisi
+# def volume_persegi (sisi):
+#     volume = luas_persegi (sisi) * sisi
+#     print("volume persegi = ", volume)
+
+# rumus sisi x sisi BISA DIBOLAK BALIK TAPI LEBIH BAGUS DIATAS AJA YAA
+# def luas_persegi (sisi):
+#     luas = sisi * sisi
+#     return luas
+
+# pemanggil fungsi
+# luas_persegi (4)
+# volume_persegi (6)
+    
+# fungsi untuk menampilkan menu
+# def show_menu():
+#     print ("\n")
+#     print ("----------- MENU---------- ")
+#     print ("[1] Show Data")
+#     print ("[2] Insert Data")
+#     print ("[3] Edit Data")
+#     print ("[4] Delete Data")
+#     print ("[5] Exit")
+
+#     menu = input("PILIH MENU> ")
+#     print ("\n")
+#     if menu == "1":
+#         show_data()
+#     elif menu == "2":
+#         insert_data()
+#     elif menu == "3":
+#         edit_data()
+#     elif menu == "4":
+#         delete_data()
+#     elif menu == "5":
+#         exit()
+#     else:
+#         print ("Salah pilih!")
+# while (True):
+#     show_menu()
